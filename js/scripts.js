fff63145dd8cfc53ae4d8d94a8ae762ca0fb31ea
gucol <ezhi@meta.ua>
2016-09-01 13:24:38 +0300

    JS+ оформление модального окна (2)

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,52 @@ var link = document.querySelector(".cont-btn");
 var popup = document.querySelector(".popup");
 var close = popup.querySelector(".close");
 
+var form = popup.querySelector(".form");
 var name = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
 
+var storage = localStorage.getItem("name");
 
 link.addEventListener("click", function(event) {
         event.preventDefault();
         popup.classList.add("popup-show");
-        name.focus();
 
+        if (storage) {
+          name.value = storage;
+          email.focus();
+        } else {
+        	name.focus();
+    }
 });
 
 close.addEventListener("click", function(event) {
         event.preventDefault();
         popup.classList.remove("popup-show");
+        popup.classList.remove("popup-error");
+
 });
+
+form.addEventListener("submit", function(event) {
+        if (!name.value || !email.value) {
+        event.preventDefault();
+        popup.classList.remove("popup-error");
+         popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("popup-error");
+ 
+        } else {
+          localStorage.setItem("name", name.value);
+        }
+});
+
+window.addEventListener("keydown", function(event) {
+        if (event.keyCode === 27) {
+ if (popup.classList.contains("popup-show")) {
+ popup.classList.remove("popup-show");
+ popup.classList.remove("popup-error");
+
+          }
+        }
+      });
+
 
 
