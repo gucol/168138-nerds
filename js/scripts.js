23d960c0f82457555bcebaf7e00a97fcbdbcf294
gucol <ezhi@meta.ua>
2016-09-03 16:15:45 +0300

    Финальная проверка. 1. Правки.

--- js/scripts.js
+++ js/scripts.js
@@ -10,34 +10,34 @@ var storage = localStorage.getItem("name");
 
 
 link.addEventListener("click", function(event) {
-    event.preventDefault();
-    popup.classList.add("popup-show");
+  event.preventDefault();
+  popup.classList.add("popup-show");
 });
 
 close.addEventListener("click", function(event) {
-    event.preventDefault();
-    popup.classList.remove("popup-show");
-    popup.classList.remove("popup-error");
+  event.preventDefault();
+  popup.classList.remove("popup-show");
+  popup.classList.remove("popup-error");
 });
 
 form.addEventListener("submit", function(event) {
-        if (!name.value || !email.value) {
-        event.preventDefault();
-        popup.classList.remove("popup-error");
-        popup.offsetWidth = popup.offsetWidth;
-        popup.classList.add("popup-error");
-        } else {
-          localStorage.setItem("name", name.value);
-        }
+  if (!name.value || !email.value) {
+  event.preventDefault();
+  popup.classList.remove("popup-error");
+  popup.offsetWidth = popup.offsetWidth;
+  popup.classList.add("popup-error");
+  } else {
+  localStorage.setItem("name", name.value);
+  }
 });
 
 window.addEventListener("keydown", function(event) {
-        if (event.keyCode === 27) {
- 			if (popup.classList.contains("popup-show")) {
- 				popup.classList.remove("popup-show");
- 				popup.classList.remove("popup-error");
-          	}
-        }
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("popup-show")) {
+    popup.classList.remove("popup-show");
+    popup.classList.remove("popup-error");
+    }
+  }
 });
 
 
